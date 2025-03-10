b3daf079ed066b170fc923b87f2ca1b621e0cfc1
Florian Berthol <fberthol@gmail.com>
2025-03-10 15:54:54 +0100

    Fin des test

--- ToRot13.php
+++ ToRot13.php
@@ -1,23 +1,15 @@
 <?php
 function transformToRot13(string $str)
 {
-    $comp = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    $alphabet =    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    $comparaison = ['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'];
     $response = [];
 
     $arrayStr = str_split($str);
-    var_dump($arrayStr);
     foreach ($arrayStr as $el) {
-        $i = array_search($el, $comp);
-        var_dump($i);
+        $i = array_search($el, $alphabet);
         if ($i !== false) {
-            $i + 1;
-            if ($i + 13 >= 26) {
-                $idx = (($i + 13) - 24);
-                var_dump($idx);
-                $response[] = $comp[$idx - 1];
-            } else {
-                $response[] = $comp[$i + 13];
-            }
+            $response[] = $comparaison[$i];
         } else {
             $response[] = ' ';
         }
--- parseToRoman.php
+++ parseToRoman.php
@@ -1,26 +1,42 @@
 <?php
-function ParseToRoman(int $number)
+function ParseToRoman(int $number , string $romanString = ''): string
 {
-    $chiffre = [
+    $chiffresRomain = [
         1000 => 'M',
+        900 => 'CM',
         500 => 'D',
+        400 => 'CD',
         100 => 'C',
+        90 => 'XC',
         50 => 'L',
+        40 => 'XL',
         10 => 'X',
+        9 => 'IX',
         5 => 'V',
+        4 => 'IV',
         1 => 'I'
     ];
-    $output = '';
-    $reste = 0;
-    foreach ($chiffre as $key => $value) {
-        if ($number > 0) {
-            exit;
+
+
+    foreach ($chiffresRomain as $key => $value) {
+        if ($number >= $key) {
+            $int = $number / $key;
+            for ($i = 1; $i <= $int; $i++) {
+                    $romanString .= $value;
+                }
+
+            $reste = $number % $key;
+            if ($reste >= 0) {
+                return ParseToRoman($reste, $romanString);
+            }
         }
-       $int = $number / $key;
-       for ($i = 1; $i <= $int; $i++) {
-           echo $chiffre[$key] . ' ';
-       }
     }
+    return $romanString;
 }
 
-parseToRoman(5);
+echo parseToRoman(1)    . '             ';
+echo parseToRoman(4)    . '             ';
+echo parseToRoman(8)    . '             ';
+echo parseToRoman(28)   . '             ';
+echo parseToRoman(1904) . '             ';
+echo parseToRoman(494)  . '             ';
